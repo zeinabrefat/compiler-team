ed23ee01c470675ef863365973f7cbd4f8828d1c
zeinabrefat <68854255+zeinabrefat@users.noreply.github.com>
2021-08-07 21:10:19 +0200

    .

--- project 16-php/index11.php
+++ project 16-php/index11.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8" />
     <title>Flights-con</title>
-    <link rel="stylesheet" href="css/stylesheet.css" type="text/css" />
+    <link rel="stylesheet" href="css/StyleSheet.css" type="text/css" />
 </head>
 <body class="no">
     <div class="ticket" id="tick">
